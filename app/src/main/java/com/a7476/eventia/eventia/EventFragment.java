d7edc8937faf1d8a72fb517cf69c24dfeda5652f
Siddharth <siddharthjain125@gmail.com>
2018-03-22 19:25:35 +0530

    Main Activity

---
+++ app/src/main/java/com/a7476/eventia/eventia/EventFragment.java
@@ -0,0 +1,30 @@
+package com.a7476.eventia.eventia;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class EventFragment extends Fragment {
+
+
+   public EventFragment(){
+
+    }
+
+    String eventId , name , category , date , time , description ;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event, container, false);
+    }
+}
