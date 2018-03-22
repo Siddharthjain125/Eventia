915c61b485ecb50ff1fbbe50d35a861ed8852a69
Siddharth <siddharthjain125@gmail.com>
2018-03-22 19:21:19 +0530

    Main Activity

--- app/src/main/java/com/a7476/eventia/eventia/MainActivity.java
+++ app/src/main/java/com/a7476/eventia/eventia/MainActivity.java
@@ -1,21 +1,29 @@
 package com.a7476.eventia.eventia;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    DatabaseReference databaseEvent ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Drawer Layout
         mDrawerLayout=(DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle= new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
 
@@ -23,6 +31,46 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Fragment
+        addFragment();
+
+    }
+/*
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseEvent.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot eventSnapshot : dataSnapshot.getChildren()){
+                    Event event = eventSnapshot.getValue(Event.class);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+*/
+
+    // Fragment
+    public void addFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        for(int i=0;i<20;i++)
+        {
+            EventFragment ef = new EventFragment();
+            fragmentTransaction.add(R.id.fragmentContainer, ef);
+        }
+
+        fragmentTransaction.commit();
+
     }
 
     @Override
