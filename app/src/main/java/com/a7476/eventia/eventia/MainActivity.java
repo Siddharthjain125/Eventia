eeddc534970c5b53a1e70fdd960b8bbfdb89ad18
Siddharth <siddharthjain125@gmail.com>
2018-03-22 21:17:26 +0530

    Logout Added

--- app/src/main/java/com/a7476/eventia/eventia/MainActivity.java
+++ app/src/main/java/com/a7476/eventia/eventia/MainActivity.java
@@ -1,27 +1,38 @@
 package com.a7476.eventia.eventia;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     DatabaseReference databaseEvent ;
+    NavigationView navigationView;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
         // Drawer Layout
         mDrawerLayout=(DrawerLayout) findViewById(R.id.drawerLayout);
@@ -30,7 +41,35 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Navigation Drawer
+
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                // Handle navigation view item clicks here.
+                switch (item.getItemId()) {
+
+                    case R.id.nav_create: {
+                        //do somthing
+                        startActivity(new Intent(MainActivity.this, CreateEventActivity.class));
+                        break;
+                    }
+
+                    case R.id.logout : {
+                        auth.signOut();
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                        finish();
+                    }
+
+                }
+                //close navigation drawer
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
 
         //Fragment
         addFragment();
@@ -73,12 +112,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        switch (item.getItemId()) {
+//
+//            case R.id.nav_create: {
+//                //do somthing
+//                Toast.makeText(this,"Create Pressed",Toast.LENGTH_LONG);
+//                break;
+//            }
+//
+//        }
+//        //close navigation drawer
+//        mDrawerLayout.closeDrawer(GravityCompat.START);
+//        return true;
+//
+//    }
+//
+//    private void setNavigationViewListener() {
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+//    }
+//
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        return false;
+//    }
 }
