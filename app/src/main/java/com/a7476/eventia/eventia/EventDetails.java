ee492861447d192913c8d508edb63e63bf71a16a
Siddharth <siddharthjain125@gmail.com>
2018-03-29 19:17:10 +0530

    Event Details Activity

--- app/src/main/java/com/a7476/eventia/eventia/EventDetails.java
+++ app/src/main/java/com/a7476/eventia/eventia/EventDetails.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 public class EventDetails extends AppCompatActivity {
 
+    private TextView tw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +18,25 @@ public class EventDetails extends AppCompatActivity {
         Event event = (Event) getIntent().getSerializableExtra("eventData");
         Toast.makeText(this,event.getName(),Toast.LENGTH_LONG).show();
 
-        TextView tw = (TextView) findViewById(R.id.name);
+        tw = (TextView) findViewById(R.id.name);
+        tw.setText(event.getName());
+
+        tw = (TextView) findViewById(R.id.CatDetails);
         tw.setText(event.getCategory());
+
+        tw = (TextView) findViewById(R.id.DateDetails);
+        tw.setText(event.getDate());
+
+        tw = (TextView) findViewById(R.id.TimeDetails);
+        tw.setText(event.getTime());
+
+        tw = (TextView) findViewById(R.id.VenueDetails);
+        tw.setText(event.getVenue());
+
+        tw = (TextView) findViewById(R.id.CityDetails);
+        tw.setText(event.getCity());
+
+        tw = (TextView) findViewById(R.id.Desc);
+        tw.setText(event.getDescription());
     }
 }
